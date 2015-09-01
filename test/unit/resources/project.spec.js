e392c6b3330f0cd5d8ab427014445027dea6c97b
Sean Gillespie <sean.gillespie@availity.com>
2015-09-01 09:03:38 -0400

    Add the /project/{projectKey} resource

---
+++ test/unit/resources/project.spec.js
@@ -0,0 +1,58 @@
+var StashClient = require('../../../lib/stash-client'),
+    nock = require('nock')
+
+describe('project', function () {
+  var project = new StashClient('http://git')
+        .coreApi()
+        .projects()
+        .project('PK-1')
+
+  it('should append /{projectKey}', function () {
+    project.url.should.equal('http://git/rest/api/1.0/projects/PK-1')
+  })
+
+  describe('deleteProject', function () {
+    var mock = nock('http://git/rest/api/1.0')
+          .delete('/projects/PK-1')
+          .reply(204)
+
+    it('should DELETE /projects/{projectKey}', function (done) {
+      project
+        .deleteProject()
+        .end()
+        .then(function () {
+          done()
+          mock.done()
+        })
+        .catch(done)
+    })
+  })
+
+  describe('updateProject', function () {
+    var opts = {
+      'key': 'PK-2',
+      'name': 'Cool project with a different name',
+      'description': 'Description for the new cooler project',
+      'avatar': 'data:image/png;base64,...',
+    }
+
+    var mock = nock('http://git/rest/api/1.0')
+          .put('/projects/PK-1', opts)
+          .reply(201, opts)
+
+    it('should PUT /projects/{projectKey}', function (done) {
+      project
+        .updateProject(opts)
+        .end()
+        .then(function (data) { return JSON.parse(data.text) })
+        .then(function (data) {
+          data.key.should.equal(opts.key)
+        })
+        .then(function () {
+          done()
+          mock.done()
+        })
+        .catch(done)
+    })
+  })
+})
--- test/unit/resources/projects.spec.js
+++ test/unit/resources/projects.spec.js
@@ -8,6 +8,10 @@ describe('projects', function () {
     projects.url.should.equal('http://git/rest/api/1.0/projects')
   })
 
+  it('project should exist', function () {
+    should.exist(projects.project('PK-1'))
+  })
+
   describe('createProject', function () {
     var opts = {
       key: 'PK',
@@ -20,7 +24,7 @@ describe('projects', function () {
           .post('/projects', opts)
           .replyWithFile(200, __dirname + '/../data/create-project-response.json')
 
-    it('should create project', function (done) {
+    it('should post to /projects', function (done) {
       new StashClient('http://git')
         .coreApi()
         .projects()
@@ -37,6 +41,37 @@ describe('projects', function () {
           data.name.should.equal(opts.name)
           data.description.should.equal(opts.description)
           data.avatar.should.equal(opts.avatar)
+        })
+        .then(function () {
+          done()
+          mock.done()
+        })
+        .catch(done)
+    })
+  })
+
+  describe('getProjects', function () {
+    var opts = {
+      name: 'name',
+      permision: 'perm',
+    }
+
+    var mock = nock('http://git/rest/api/1.0')
+          .get('/projects')
+          .query(opts)
+          .replyWithFile(200, __dirname + '/../data/get-projects-response.json')
+
+    it('should GET /projects', function (done) {
+      new StashClient('http://git')
+        .coreApi()
+        .projects()
+        .getProjects(opts)
+        .end()
+        .then(function (resp) { return JSON.parse(resp.text) })
+        .get('values')
+        .then(function (data) {
+          data[0].key.should.equal('PK-1')
+          data[1].key.should.equal('PK-2')
         })
         .then(function () {
           done()
