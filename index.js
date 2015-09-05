505dcec518921e67acd035421e94655494bd9e4f
Sean Gillespie <sean.gillespie@availity.com>
2015-09-05 10:29:57 -0400

    Remove useless references

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-var CoreApi = require('./lib/resources/CoreApi'),
-    Project = require('./lib/resources/CoreApi'),
-    Projects = require('./lib/resources/CoreApi'),
-    RestClient = require('./lib/RestClient'),
-    StashClient = require('./lib/StashClient')
+var StashClient = require('./lib/StashClient')
 
 function stashClient (baseUrl) {
   return new StashClient(baseUrl)
 }
-
-stashClient.CoreApi = CoreApi
-stashClient.Project = Project
-stashClient.Projects = Projects
-stashClient.RestClient = RestClient
-stashClient.StashClient = StashClient
 
 module.exports = stashClient
--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -8,24 +8,4 @@ describe('stash-api-client', function () {
     stash.url.should.equal('http://git')
     should.exist(stash.coreApi())
   })
-
-  it('RestClient should exist', function () {
-    should.exist(new stashApiClient.RestClient())
-  })
-
-  it('StashClient should exist', function () {
-    should.exist(new stashApiClient.StashClient())
-  })
-
-  it('CoreApi should exist', function () {
-    should.exist(new stashApiClient.CoreApi())
-  })
-
-  it('Project should exist', function () {
-    should.exist(new stashApiClient.Project())
-  })
-
-  it('Projects should exist', function () {
-    should.exist(new stashApiClient.Projects())
-  })
 })
