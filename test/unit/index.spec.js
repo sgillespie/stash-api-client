4ea6a9c61e57e67fa9581992170b32ad62371d8d
Sean Gillespie <sean.gillespie@availity.com>
2015-09-06 00:21:50 -0400

    Revert "Break the build"
    This reverts commit a96f3cbbae7f9efdbc4b523491f3e1579dce4230.

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -8,8 +8,4 @@ describe('stash-api-client', function () {
     stash.url.should.equal('http://git')
     should.exist(stash.coreApi())
   })
-
-  it('should break the build', function () {
-    should.exist(undefined)
-  })
 })
